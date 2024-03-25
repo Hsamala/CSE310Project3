a6fc1a8a6efeb017205b9dad456379ff94dd318f
Herald Actual <heraldsComputer@anything.com>
2024-03-24 20:50:53 -0700

    added files

--- data-structures.h
+++ data-structures.h
@@ -6,11 +6,10 @@ enum COLOR {White, Black, Gray};
 typedef struct TAG_VERTEX{
     int index;
     COLOR color;
-    double key;
+    double key; //Must update everytime we relax the graph
     int pi;
     int position;
 } VERTEX;
 
-//This is a test of github
 
 #endif
