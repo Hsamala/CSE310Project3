e8b2af573bab53d7e35eed7257e5dc8e28af89e0
Herald Samala <heraldsamala@MacBook-Pro.local>
2024-03-24 19:34:54 -0700

    Add existing file

---
+++ data-structures.h
@@ -0,0 +1,14 @@
+#ifndef _data_structures_h
+#define _data_structures_h 1
+
+enum COLOR {White, Black, Gray};
+
+typedef struct TAG_VERTEX{
+    int index;
+    COLOR color;
+    double key;
+    int pi;
+    int position;
+} VERTEX;
+
+#endif
