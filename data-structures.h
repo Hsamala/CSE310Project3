183bef9a2b69bb0f45a78aaea3504b7d06324a78
Herald Samala <heraldsamala@MacBook-Pro.local>
2024-03-24 19:50:13 -0700

    test commit2

---
+++ data-structures.h
@@ -0,0 +1,16 @@
+#ifndef _data_structures_h
+#define _data_structures_h 1
+
+enum COLOR {White, Black, Gray};
+
+typedef struct TAG_VERTEX{
+    int index;
+    COLOR color;
+    double key;
+    int pi;
+    int position;
+} VERTEX;
+
+//This is a test of github
+
+#endif
