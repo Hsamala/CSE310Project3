9de0ebb4c9e2ba0330bde497ef3297fb1391e9df
Herald Actual <heraldsComputer@anything.com>
2024-03-24 21:03:04 -0700

    data structures header file

--- data-structures.h
+++ data-structures.h
@@ -1,2 +1,13 @@
+#ifndef _data_structures_h
+#define _data_structures_h 1
+
+enum COLOR {White, Gray, Black};
+typedef struct TAG_VERTEX{
+    int index;
+    COLOR color;
+    double key;
+    int pi;
+    int position;
+}VERTEX;
 
 #endif
